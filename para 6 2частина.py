5a927dd9c336709caf00f4c18cab98ce07243332
Igor11234 <Gric_ku70@student.itstep.org>
2024-04-27 17:50:16 +0300

    added new super program

--- para 6 2частина.py
+++ para 6 2частина.py
@@ -1,15 +1,53 @@
-class AudioPlayer:
-    def play_audio(self):
-        print("Playing audio...")
+# class AudioPlayer:
+#     def play_audio(self):
+#         print("Playing audio...")
+#
+# class VideoPlayer:
+#     def play_video(self):
+#         print("Playing video...")
+#
+# class MultimediaPlayer(AudioPlayer, VideoPlayer):
+#     def play(self):
+#         self.play_audio()
+#         self.play_video()
+#         print("Playing multimedia content...")
+# player = MultimediaPlayer()
+# player.play()
 
-class VideoPlayer:
-    def play_video(self):
-        print("Playing video...")
 
-class MultimediaPlayer(AudioPlayer, VideoPlayer):
-    def play(self):
-        self.play_audio()
-        self.play_video()
-        print("Playing multimedia content...")
-player = MultimediaPlayer()
-player.play()
+
+
+
+
+
+
+
+
+
+
+
+class Engine:
+    def start(self):
+        print("Двигун запущено")
+
+    def charge(self):
+        pass
+
+
+class ElectricVehicle:
+    def start(self):
+        pass
+
+    def charge(self):
+        print("Процес зарядки")
+
+
+class HybridCar(Engine, ElectricVehicle):
+    def drive(self):
+        print("Гібридний автомобіль рухається")
+
+
+car = HybridCar()
+car.start()
+car.charge()
+car.drive()
