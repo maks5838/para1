29fb83d2429b3d950e956b6da888168c298aa55e
Igor11234 <Gric_ku70@student.itstep.org>
2024-05-18 16:23:35 +0300

    added new super program

---
+++ дз 18.05.py
@@ -0,0 +1,6 @@
+import colorama
+from colorama import Fore, Back, Style
+print(dir(colorama))
+print("Fore атрибути:", dir(Fore))
+print("Back атрибути:", dir(Back))
+print("Style атрибути:", dir(Style))
