bbcff7496d5d3f74a7e0f4b25f462355787a0a7f
Igor11234 <Gric_ku70@student.itstep.org>
2024-05-18 15:13:31 +0300

    added new super program

--- para 8.py
+++ para 8.py
@@ -46,3 +46,46 @@
 # material = 120
 # checker(material,   450)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import colorama
+from colorama import Fore, Back, Style
+print(dir(colorama))
+print("Fore атрибути:", dir(Fore))
+print("Back атрибути:", dir(Back))
+print("Style атрибути:", dir(Style))
